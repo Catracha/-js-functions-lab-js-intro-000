373a885a80f63995ed3b58a5f48a62f261b19884
catracha <jeder.ramosaguilar2018@gmail.com>
2016-11-01 18:08:15 +0000

    I'm ready to send my files to github.

---
+++ index.js
@@ -0,0 +1,7 @@
+// write your code below!
+var (happyHolidays) = function(person){
+  return ("Happy holidays!");
+};
+var (happyHolidaysTo('you') = function (Happy holidays, you!');
+  return ("Happy holidays,${name}!");
+
